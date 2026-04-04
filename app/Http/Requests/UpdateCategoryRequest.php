5aa58af55ed22a64862d56176711fb6606a086ed
Axel Raboit <axel.raboit@gmail.com>
2026-04-04 10:33:15 +0200

    refactor: extract validation and authorization to Form Request classes

---
+++ app/Http/Requests/UpdateCategoryRequest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateCategoryRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return $this->user()->can('update', $this->route('category'));
+    }
+
+    public function rules(): array
+    {
+        return [
+            'name' => ['required', 'string', 'max:255'],
+        ];
+    }
+}
