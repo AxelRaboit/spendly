85eaec3c798be02325087bebfec50cc980e7694e
Axel Raboit <axel.raboit@gmail.com>
2026-04-11 15:40:02 +0200

    chore: increase chunkSizeWarningLimit by 1000 on vite config because of xlsx-js-style lib

--- vite.config.js
+++ vite.config.js
@@ -18,6 +18,7 @@ export default defineConfig({
         }),
     ],
     build: {
+        chunkSizeWarningLimit: 1000,
         rolldownOptions: {
             output: {
                 manualChunks(id) {
